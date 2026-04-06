508fae092a72d79e7476d772dc6566717f74b948
GustavoSerra1 <gustavoserradacosta@gmail.com>
2026-04-06 19:02:49 -0300

    Add files via upload

---
+++ Números positivos e negativos.py
@@ -0,0 +1,8 @@
+#Número positivo ou negativo
+n1 = int(input("Digite um número:"))
+if (n1>0):
+    print ("Esse número é positivo")
+elif (n1==0):
+    print ("Esse número é 0")
+else:
+    print ("Esse número é negativo")
